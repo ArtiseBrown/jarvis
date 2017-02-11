c39f097ad098d6bf8cdecc55d48b686c3d7cad01
ArtiseBrown <artise.brown@gmail.com>
2017-02-11 15:59:50 +0000

    Update chroot-install.sh
    Added the use of sed to un-comment %wheel group in /etc/sudoers.
    
    Removed more debugging code for sections that are working as expected.

--- chroot-install.sh
+++ chroot-install.sh
@@ -6,7 +6,7 @@ echo "Setting the time zone and UTC"
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc --utc
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 echo ""
 
 ## Set the localizations
@@ -16,7 +16,7 @@ echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 echo "KEYMAP=uk" > /etc/vconsole.conf
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 echo ""
 
 ## Set the computer's hostname and network access
@@ -26,7 +26,7 @@ echo "127.0.0.1	localhost.localdomain	localhost" > /etc/hosts
 echo "1::1		localhost.localdomain	localhost" >> /etc/hosts
 echo "127.0.1.1	hulk.localdomain	hulk" >> /etc/hosts
 systemctl enable dhcpcd.service
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 echo ""
 
 ## Setup the boot loader and conf files
@@ -35,7 +35,7 @@ bootctl --path=/boot install
 echo "default arch" > /boot/loader/loader.conf
 echo "timer 0" >> /boot/loader/loader.conf
 echo "editor 0" >> /boot/loader/loader.conf
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 echo ""
 
 ## determine the PARTUUID of /dev/sda1
@@ -46,16 +46,27 @@ echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd  /intel-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=PARTUUID=$DISKID rw quiet" >> /boot/loader/entries/arch.conf
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 echo ""
 
 ## Add password for root, add user and update sudoers
 echo "Setting the root password"
 passwd
-#read -p "Press enter to continue"
+echo ""
+
+#uncomment # %wheel ALL=(ALL) ALL in the /etc/sudoers file
+echo "Uncommenting %wheel in sudoers file"
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+echo ""
+
+echo "Adding a non-root user"
+useradd -m -G wheel,storage,power -s /usr/bin/fish artise
+echo ""
+
+echo "Adding password for non-root user"
+passwd artise
+echo ""
+
 echo "Basic installation complete"
 
-#uncomment %wheel ALL=(ALL) ALL in the /etc/sudoers file
-#useradd -m -G wheel,storage,power -s /usr/bin/fish artise
-#passwd artise
 exit # to leave the chroot
