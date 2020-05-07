3b5d700bb185d27ee1049dc2cacc7816d3bde9e4
ArtiseBrown <artise.brown@gmail.com>
2020-05-07 16:22:46 +0100

    Update arch-install.sh
    Further updates to get the script ready for production

--- arch-install.sh
+++ arch-install.sh
@@ -14,211 +14,165 @@
 
 ## Pre-installation tasks
 ### Set the keyboard map
-echo "Setting the keyboard to the UK layout"
+echo "Setting the keyboard to the UK layout..."
 loadkeys uk
-#read -p "Press enter to continue"
 echo ""
 
 ### Set the system clock
+echo "Setting network time..."
 timedatectl set-ntp true
+echo ""
 
 ### Update the mirrorlist
-#echo "Updating the mirrorlist..."
+echo "Updating the mirrorlist..."
 pacman -Sy reflector
 reflector --country 'United Kingdom' --latest 10 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist # Grab list of fastest mirrors
 echo "Mirrorlist updated."
 echo ""
 
 
-## Determin the drive name that Arch will be install on
-## lsblk
-
 ## Partition the drive that you will use
-echo "Partitioning the disk"
+echo "Partitioning the disk..."
 sgdisk -Z /dev/sda
 #sgdisk -Z /dev/nvme0n1
-echo "Creating the boot partition"
+echo "Creating the boot partition..."
 sgdisk -n 1:0:+512M -t 1:ef00 -c 1:UEFI /dev/sda
 #sgdisk -n 1:0:+512M -t 1:ef00 -c 1:UEFI /dev/nvme0n1
-echo "Creating the root partition"
+echo "Creating the root partition..."
 sgdisk -n 2:0:+30G -t 2:8300 -c 2:Arch /dev/sda
 #sgdisk -n 2:0:+30G -t 2:8300 -c 2:Arch /dev/nvme0n1
-echo "Creating the home partition"
+echo "Creating the home partition..."
 sgdisk -n 3:0:0 -t 3:8300 -c 3:Home /dev/sda
 #sgdisk -n 3:0:0 -t 3:8300 -c 3:Home /dev/nvme0n1
-#read -p "Press enter to continue"
 echo ""
 
 ## Format the partitions
-echo "Formating the boot partition"
+echo "Formating the boot partition..."
 mkfs.fat -F32 /dev/sda1
 #mkfs.fat -F32 /dev/nvme0n1p1
-echo "Formating the root partition"
+echo "Formating the root partition..."
 mkfs.ext4 /dev/sda2
 #mkfs.ext4 /dev/nvme0n1p2
-echo "Formating the home partition"
+echo "Formating the home partition..."
 mkfs.ext4 /dev/sda3
 #mkfs.ext4 /dev/nvme0n1p3
-#read -p "Press enter to continue"
 echo ""
 
 ## Mount partitions
-echo "Mounting the root partition"
+echo "Mounting the root partition..."
 mount /dev/sda2 /mnt
 #mount /dev/nvme0n1p2 /mnt
-echo "Creating the boot folder and mounting the boot partition"
+echo "Creating the boot folder and mounting the boot partition..."
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 #mount /dev/nvme0n1p1 /mnt/boot
-echo "Creating the homer folere and mounting the home partition"
+echo "Creating the homer folere and mounting the home partition..."
 mkdir /mnt/home
 mount /dev/sda3 /mnt/home
 #mount /dev/nvme0n1p3 /mnt/home
-#read -p "Press enter to continue"
 echo ""
 
 ## Install base system
-echo "Intalling the base system"
-pacstrap /mnt base base-devel intel-ucode 
-#read -p "Press enter to continue"
+echo "Intalling the base system..."
+pacstrap /mnt base base-devel intel-ucode linux linux-headers linux-lts linux-lts-headers linux-firmware grub efibootmgr ntp networkmanager python docker nano
 echo ""
 
 ## Create the file system table
-echo "Creating the file system table"
+echo "Creating the file system table..."
 genfstab -U /mnt >> /mnt/etc/fstab
-#read -p "Press enter to continue"
 echo ""
 
 ## Chroot into the new system abd run the chroot-install script
-echo "Copying the chroot-install.sh to the root folder"
-cp ./chroot-install.sh /mnt/chroot-install.sh
-chmod +x /mnt/chroot-install.sh
-echo "Copying the user-install.sh to the root folder"
-cp ./user-install.sh /mnt/user-install.sh
-chmod +x /mnt/user-install.sh
-echo "Chrooting into the new system"
-arch-chroot /mnt /chroot-install.sh
-read -p "Press enter to continue"
+arch-chroot /mnt
 echo ""
 
-## Get access to the AUR
-#su artise
-#cd /tmp
-#git clone https://aur.archlinux.org/package-query.git
-#cd /tmp/package-query
-#makepkg -si
-#cd /tmp
-#git clone https://aur.archlinux.org/yaourt.git 
-#cd /tmp/yaourt
-#makepkg -si
-#yourt -Syua
-
-## Add plymouth to the initial boot screen
-
-## Setup the login manager
-#yaourt -S lightdm-webkit2-greeter --noconfirm
-#sudo systemctl enable lightdm.service
+## Set the timezone and hardware clock
+echo "Setting the time zone and UTC..."
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc --utc
+echo ""
 
 
+## Set the localizations
+echo "Setting the localisations to the UK..."
+echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+echo "KEYMAP=uk" > /etc/vconsole.conf
+echo ""
 
 
-## Add archlinuxfr to pacman.conf
-# [archlinuxfr]
-# SigLevel = Never
-# Server = http://repo.archlinux.fr/$arch
+## Set the computer's hostname and network access
+echo "Setting the host name details..."
+read -p "Set a hostname: " hostnamevar
+echo $hostnamevar > /etc/hostname
+echo "127.0.0.1	localhost.localdomain	localhost" > /etc/hosts
+echo "1::1		localhost.localdomain	localhost" >> /etc/hosts
+echo "127.0.1.1	$hostnamevar.localdomain	$hostnamevar" >> /etc/hosts
+echo ""
+
+
+## Configure initramfs
+echo "Configuring initramfs..."
+mkinitcpio -P
+echo ""
+
+
+## Services to enable and start
+echo "Enabling services..."
+systemctl enable NetworkManager.service
+systemctl enable ntpd.service
+echo ""
+
+## Set password for root 
+echo "Setting the root password:"
+passwd
+echo ""
+
+
+## Add a user
+echo "Add system user:"
+read -p 'Username: ' usernamevar
+useradd -m -G wheel,storage,power -s /usr/bin/fish $usernamevar
+passwd $usernamevar
+echo ""
+
+
+# uncomment # %wheel ALL=(ALL) ALL in the /etc/sudoers file
+echo "Uncommenting %wheel in sudoers file"
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+echo ""
+#echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+
+
+
+## Setup the boot loader and conf files
+echo "Configuring the bootloader"
+bootctl --path=/boot install
+echo "default arch" > /boot/loader/loader.conf
+echo "timeout 0" >> /boot/loader/loader.conf
+echo "editor no" >> /boot/loader/loader.conf
+#read -p "Press enter to continue"
+echo ""
+
+## Add hooks to update systemd-boot
+mkdir /etc/pacman.d/hooks
+echo "[Trigger]" > /etc/pacman.d/hooks/systemd-boot.hook
+echo "Type = Package" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "Operation = Upgrade" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "Target = systemd" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "[Action]" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "Description = Upgrading systemd-boot..." >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "When = PostTransaction" >> /etc/pacman.d/hooks/systemd-boot.hook
+echo "Exec = /usr/bin/bootctl update" >> /etc/pacman.d/hooks/systemd-boot.hook
 
 
 
 
-# Software configurations
-## i3
-### ~/.i3/config
-#### Software startup
-#     exec --no-startup-id "pulseaudio --start
-#     exec --no-startup-id nm-applet
-#     exec --no-startup-id feh --bg-fill ~/Pictures/Dark-pattern.jpg
-#     exec --no-startup-id dropbox start
-#     exec --no-startup-id "setxkbmap -model pc105 -layout gb"
-#     exec --no-startup-id pamac-tray
-     
-#### Multimedia keys
-# Multimedia Keys
-# increase volume
-# bindsym XF86AudioRaiseVolume exec amixer -q set Master 5%+ unmute
-# decrease volume
-# bindsym XF86AudioLowerVolume exec amixer -q set Master 5%- unmute
-# mute volume
-# bindsym XF86AudioMute exec amixer -q set Master mute
-# pause / play / next / previous
-# bindsym XF86AudioPlay exec playerctl play-pause
-# bindsym XF86AudioNext exec playerctl next
-# bindsym XF86AudioPrev exec playerctl previous
 
-#### Other key bindings
-# bindsym $mod+d exec --no-startup-id i3-dmenu-desktop
-# bindsym Print exec scrot -e 'mv $f ~/Pictures/Screenshots'
 
-### ~/.i3status
-#    order += "volume master"
-#    ...
-#    ...
-#    ...
 
-#    volume master {
-#        format = "V: %volume"
-#        device = "default"
-#        mixer = "Master"
-#        mixer_idx = 0
-#	}
-    
-## Terminator
-   
-## Thunar
-## Rofi
-## Lightdm
-### Add your avatar to the login screen
-#    Eure the accountsservice package from the official repositories is installed
-#    
-#    Create a file /var/lib/AccountsService/users/myUserName
-#    add the following to it:
-#      [User]
-#      Icon=/var/lib/AccountsService/icons/myUserName
-#
-#    Create the file /var/lib/AccountsService/icons/myUserName using a 
-#    96x96 PNG image file.
-#
-#    Note: Make sure that both created files have 644 permissions, 
-#    use chmod to correct them
-#
-#    To convert a jpg with the correct dimensions do the following:
-#      sudo convert artise.jpg -resize 96x96 artise.png
 
-# Config files to keep copies of
-#  .emacs
-#  .config/i3/config
-#  .confif/terminatir/config
-#  .config/Thunar/???
 
-# Linking folders in my home folder
-## Make a change to the $HOME/.config/user-dirs.dirs
-#   Replace XDG_PICTURES_DIR="$HOME/Wallpaper"
-#   Replace XDG_TEMPLATES_DIR="$HOME/GTD"
-
-## Run the following commands:
-### Remove unused folders:
-# rm -rf $HOME/Desktop
-# rm -rf $HOME/Documents       
-# rm -rf $HOME/Downloads    
-# rm -rf $HOME/Pictures
-# rm -rf $HOME/Music
-# rm -rf $HOME/Templates
-# rm -rf $HOME/Videos
-
-### Link to Dropbox folders:
-# ln -s /home/artise/Dropbox/documents /home/artise
-# ln -s /home/artise/Dropbox/Downloads /home/artise
-# ln -s /home/artise/Dropbox/wallpaper /home/artise
-# ln -s /home/artise/Dropbox/gtd /home/artise
-# ln -s /home/artise/Dropbox/arch-system-config/_thor/i3 /home/artise/.config/i3
-
-# Useful commands
